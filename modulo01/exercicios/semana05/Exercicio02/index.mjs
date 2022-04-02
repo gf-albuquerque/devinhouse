b276950368847656b5f582a6b71bcad09f0bf2f7
Gabriel F Albuquerque <gfonteles.a@gmail.com>
2022-04-01 21:33:49 -0300

    Resolução exercicio 02

---
+++ modulo01/exercicios/semana05/Exercicio02/index.mjs
@@ -0,0 +1,5 @@
+import { saudacao } from "./saudacao.mjs";
+import saudacaoEspecial from "./saudacao-especial.mjs";
+
+console.log(saudacao());
+console.log(saudacaoEspecial("gabriel albuquerque"));
---
+++ modulo01/exercicios/semana05/Exercicio02/saudacao-especial.mjs
@@ -0,0 +1,4 @@
+export default function novaSaudacao(nome) {
+  const saudar = `Olá, ${nome}! Seja muito bem vindo!`;
+  return saudar;
+}
